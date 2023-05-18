babbc95266b3bd29b48cbc776afae8764fce5ea6
Mustafizur Rohman <mustafizur.rohman88@gmail.com>
2023-05-18 17:57:06 +0200

    Added more unit tests

--- AutoFixture.Demo.Tests/UnitTests/ContactTests.cs
+++ AutoFixture.Demo.Tests/UnitTests/ContactTests.cs
@@ -1,11 +1,12 @@
-﻿namespace AutoFixture.Demo.Tests.UnitTests;
+﻿using AutoFixture.Xunit2;
+
+namespace AutoFixture.Demo.Tests.UnitTests;
 
 public class ContactTests
 {
     [Theory]
-    [AutoDataCustom("fr")]
-    // [AutoDataContact]
-    public void VerifyThatContactsAreCorrectlyGenerated(List<Contact> contacts)
+    [AutoData]
+    public void VerifyThatContactsAreCorrectlyGeneratedWithoutFixtureCustomization(List<Contact> contacts)
     {
         using (new AssertionScope())
         {
@@ -13,6 +14,44 @@ public class ContactTests
                 .Should()
                 .NotBeNull();
         }
+    }
+
+    [Theory]
+    [AutoDataCustom("it")]
+    // [AutoDataContact]
+    public void VerifyThatContactsAreCorrectlyGeneratedWithFixtureCustomization(List<Contact> contacts)
+    {
+        using (new AssertionScope())
+        {
+            contacts
+                .Should()
+                .NotBeNull();
+        }
+    }
+
+    [Fact]
+    public void DemoContactGenerationWithoutFixtureCustomization()
+    {
+        var fixture = new Fixture();
+        var personBuilder = fixture.Build<Person>();
+
+        var persons = personBuilder.CreateMany()
+            .ToList();
+
+        persons.Should().NotBeNull();
+    }
+
+    [Fact]
+    public void DemoContactGenerationWithFixtureCustomization()
+    {
+        var fixture = new Fixture();
+        fixture.Customize(new AllCustomization());
+        var personBuilder = fixture.Build<Person>();
+
+        var persons = personBuilder.CreateMany()
+            .ToList();
+
+        persons.Should().NotBeNull();
     }
 }
 
