626b0fa68958a61343a3d13cb2c3f7f6583d1c8a
Mustafizur Rohman <mustafizur.rohman88@gmail.com>
2023-05-26 15:33:53 +0200

    Updated PersonAssertions

--- AutoFixture.Demo.Tests/AssertionHelpers/PersonAssertions.cs
+++ AutoFixture.Demo.Tests/AssertionHelpers/PersonAssertions.cs
@@ -5,10 +5,10 @@ namespace AutoFixture.Demo.Tests.AssertionHelpers;
 
 public static partial class PersonAssertions
 {
-    [GeneratedRegex("[\u0000-\u007Fäüößé]+ [\u0000-\u007Fäüößé]+", RegexOptions.Compiled)]
+    [GeneratedRegex("[\u0000-\u007FäüößéÄÜÖ]+ [\u0000-\u007FäüößéÄÜÖ]+", RegexOptions.Compiled)]
     private static partial Regex FullNameRegex();
 
-    [GeneratedRegex("[\u0000-\u007Fäüößé][\u0000-\u007Fäüößé]+", RegexOptions.Compiled)]
+    [GeneratedRegex("[\u0000-\u007FäüößéÄÜÖ][\u0000-\u007FäüößéÄÜÖ]+", RegexOptions.Compiled)]
     private static partial Regex NameRegex();
 
     public static void ShouldBeValidPerson(this Person person)
